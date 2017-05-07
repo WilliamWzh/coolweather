68494a1dc0e5c5295c90fc4616de535930f21b3b
wzhworld <wzh15521173239@gmail.com>
2017-05-07 16:41:12 +0800

    加入创建数据库和表的各项配置“

---
+++ app/src/main/java/com/example/wzhworld/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.wzhworld.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by wzhworld on 2017/5/7.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
